261c4c4aaafe77cc5dd97323a7df50f81e841a1b
Daniel Dao <danieldkdao@gmail.com>
2026-09-19 15:05:50 -0500

    feat: setup authentication

--- src/data/env/server.ts
+++ src/data/env/server.ts
@@ -4,6 +4,12 @@ import z from "zod";
 export const envServer = createEnv({
   server: {
     DATABASE_URL: z.string().min(1),
+    BETTER_AUTH_SECRET: z.string().min(1),
+    BETTER_AUTH_URL: z.url().min(1),
+    GOOGLE_CLIENT_ID: z.string().min(1),
+    GOOGLE_CLIENT_SECRET: z.string().min(1),
+    GITHUB_CLIENT_ID: z.string().min(1),
+    GITHUB_CLIENT_SECRET: z.string().min(1),
     APP_URL: z.string().min(1),
     MAILJET_API_KEY: z.string().min(1),
     MAILJET_API_SECRET: z.string().min(1),
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,1 +1,2 @@
 export * from "./schemas/session";
+export * from "./schemas/user";
